578070e05815331015a07a50efbde535ee45d533
Ahmed Shaalan <ahmedshaalan2006@gmail.com>
2024-09-03 16:17:14 +0200

    Created the project

---
+++ Main/main.cpp
@@ -0,0 +1,13 @@
+#include "../Declarations/declarations.h"
+
+// RIFF CHUNK
+string ChunkID = "RIFF";
+string ChunkSize = "----";
+string Format = "WAVE";
+
+// FMT SUB_CHUNK
+int main () {
+    
+    
+    return 0;
+}
